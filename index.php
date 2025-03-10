516705a468031c2c45783284d4d8f9cf03a2e0f4
Oumou2003 <oumoukhairythiandoum18@gmail.com>
2025-03-10 19:15:59 +0100

    Add files via upload

--- index.php
+++ index.php
@@ -1,5 +1,20 @@
-<?php
-// Redirige vers la liste des documents
-header("Location: read_documents.php");
-exit();
-?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>SmartTech App</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <h1>Bienvenue sur SmartTech App</h1>
+        <ul>
+            <li><a href="clients/">Clients</a></li>
+            <li><a href="documents/">Documents</a></li>
+            <li><a href="employes/">Employés</a></li>
+            
+        </ul>
+    </div>
+</body>
+</html>
